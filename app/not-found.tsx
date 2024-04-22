4be3dab27da2ec02e6b07045a4050c336306002a
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-04-22 17:13:20 +0500

    chore: update not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,26 @@
 import Link from 'next/link';
 import { Routes } from './routes';
+import RedirectLink from '@/app/ui/redirect-link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href={Routes.HOME}>Return Home</Link>
+    <div className="relative grid place-items-center px-8 py-20">
+      <div>
+        <p className="text-[100px] text-neutral-500 opacity-10 sm:text-[200px] md:text-[300px] lg:text-[400px]">
+          404
+        </p>
+      </div>
+      <div className="absolute grid place-items-center text-center">
+        <h2 className="text-display-2 font-bold">Page not Found</h2>
+        <p className="text-body-large mb-6">
+          It seems the page you’re looking for not exists.
+        </p>
+        <RedirectLink
+          href={Routes.HOME}
+          label="Return Home"
+          className="text-xl sm:text-2xl"
+        />
+      </div>
     </div>
   );
 }
