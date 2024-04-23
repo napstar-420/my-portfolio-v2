97abdfcb7639de796982e1346c6ecf7b7ad1230c
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-04-23 21:47:29 +0500

    rename

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import 'aos/dist/aos.css';
 import Hero from '@/app/ui/home/hero';
 import About from '@/app/ui/home/about';
 import Skills from '@/app/ui/home/skills';
-import Portfolio from '@/app/ui/home/portfolio';
+import Portfolio from '@/app/ui/home/projects';
 import Testimonials from '@/app/ui/home/testimonials';
 import Contact from '@/app/ui/home/contact';
 
---
+++ app/ui/Home/projects.tsx
@@ -0,0 +1,35 @@
+import RedirectLink from '@/app/ui/redirect-link';
+import portfolioItems from '@/app/lib/portfolio';
+import PortfolioCard from '../portfolio-card';
+import { Routes } from '@/app/routes';
+import Section from '@/app/ui/section';
+
+export default function Portfolio() {
+  return (
+    <Section secondaryBg>
+      <div className="grid gap-8 px-5 py-24 md:grid-cols-2 md:gap-10">
+        <div>
+          <div className="mb-16" data-aos="fade-up">
+            <h2 className="subtitle mb-4">
+              <span className="text-primary">/</span>&nbsp;&nbsp;My Portfolio
+            </h2>
+            <h3 className="text-display-3 mb-8">
+              Take a look at the latest projects I’ve done
+            </h3>
+            <RedirectLink
+              label="Browser all projects"
+              href={Routes.PORTFOLIO}
+              className="underline-large"
+            />
+          </div>
+          <PortfolioCard {...portfolioItems[0]} />
+        </div>
+        <div className="grid gap-8 md:gap-10">
+          {portfolioItems.slice(1).map((item, index) => (
+            <PortfolioCard key={index} {...item} />
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
+}
