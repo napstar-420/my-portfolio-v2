7c5c9d26c52348657e711cf0e0250ca2da96a395
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-04-24 23:29:30 +0500

    chore: build my experiences

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/app/ui/about/hero';
 import MyStory from '@/app/ui/about/my-story';
+import MyExperience from '@/app/ui/about/my-experience';
 
 export default function About() {
   return (
     <main>
       <Hero />
       <MyStory />
+      <MyExperience />
     </main>
   );
 }
---
+++ app/lib/experiences.ts
@@ -0,0 +1,24 @@
+const experiences = [
+  {
+    id: 1,
+    title: 'Frontend engineer',
+    company: 'Plotano',
+    location: 'Lahore, PK',
+    startDate: 'JAN 2023',
+    endDate: 'APR 2023',
+    description:
+      'Worked with a team of 3 to develop projects for the clients. Developed a web game using PixiJS and Socket.io. Implemented realtime multiplayer functionality and optimized the game for performance. Started the project from scratch and delivered it within the deadline.',
+  },
+  {
+    id: 2,
+    title: 'Fullstack engineer',
+    company: 'Freedom',
+    location: 'Los Angeles, US',
+    startDate: 'APR 2023',
+    endDate: 'Present',
+    description:
+      'Worked on the frontend and backend of the web applications. Worked on 3 projects Photos, Moments and Studio. Developed the frontend of the Photos project using Vue and Pinia. Developed the backend of the Moments project using NestJS and Knex for the database. Developed the Studio project using Quasar and NestJS. Worked with a team of 7 to deliver the projects features and fix the bugs.',
+  },
+];
+
+export default experiences;
