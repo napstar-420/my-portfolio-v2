d58276c0a972586a40864f8d62430f8a3229f66d
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-04-25 19:47:50 +0500

    chore: update section component

--- app/ui/section.tsx
+++ app/ui/section.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { HTMLProps } from 'react';
 import clsx from 'clsx';
 
 export default function Section({
   children,
   secondaryBg,
   className,
-}: {
+  ...props
+}: HTMLProps<HTMLElement> & {
   children: React.ReactNode;
   secondaryBg?: boolean;
   className?: string;
 }) {
   return (
     <section
+      {...props}
       className={clsx(
         'px-5 py-24 sm:py-28 md:py-40 lg:py-60',
         className,
