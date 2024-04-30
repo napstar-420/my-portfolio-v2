9735a2e41f93bc604f2cde519e4ae2f3cb9aee4a
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-05-01 04:13:49 +0500

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,28 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Zohaib Portfolio',
-  description: 'Under development',
+  description:
+    "Welcome to Zohaib's Portfolio! Explore my projects, skills, and experience.",
+  keywords: [
+    'Zohaib',
+    'Portfolio',
+    'Coding',
+    'Fullstack developer',
+    'Web developer',
+    'Software engineer',
+  ],
+  authors: [{ name: 'Zohaib', url: 'https://zohaib.codes' }],
+  applicationName: 'Zohaib Portfolio',
+  category: 'Portfolio',
+  creator: 'Zohaib',
+  metadataBase: new URL('https://zohaib.codes'),
+  openGraph: {
+    title: 'Zohaib Portfolio',
+    description:
+      "Welcome to Zohaib's Portfolio! Explore my projects, skills, and experience.",
+    type: 'website',
+    siteName: 'Zohaib Portfolio',
+  },
 };
 
 export default function RootLayout({
